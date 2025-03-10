d96c5b0840af55d26c6f20a3581e36d7d89e0467
Aliena4312 <bahacutiee@gmail.com>
2025-03-10 20:18:27 +0300

    Add files via upload

---
+++ Do-while loop 1.cpp
@@ -0,0 +1,22 @@
+/*
+do-while loops
+Author:Aliena Mathei
+Reg no:D33-4312-2023
+Date:10-3-2025
+
+*/
+#include <stdio.h>
+
+int main() {
+	int i=1; // start 
+	do{
+	printf("%d \n", i*i*i);
+	i++; 
+	
+	
+	
+	}while(i<=5);
+	
+	return 0; // successful execution
+}
+	
